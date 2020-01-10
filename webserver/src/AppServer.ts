00f79821adee5492c9ab10bfb9f1792638702f2d
Noah Crowley <noahwillcrow@gmail.com>
2020-01-10 10:40:59 -0800

    Change setupControllers to be more explicit

--- webserver/src/AppServer.ts
+++ webserver/src/AppServer.ts
@@ -1,9 +1,9 @@
 // Thanks to the following blog article for all the help: https://levelup.gitconnected.com/setup-express-with-typescript-in-3-easy-steps-484772062e01
 
 import * as bodyParser from 'body-parser';
-import * as controllers from './controllers';
 import { Server } from '@overnightjs/core';
 import { Logger } from '@overnightjs/logger';
+import { AttentionController } from './controllers/AttentionController';
 
 export class AppServer extends Server {
 	public static readonly port: number = 3000;
@@ -17,13 +17,10 @@ export class AppServer extends Server {
 	}
 
 	private setupControllers(): void {
-        const ctlrInstances = [];
-        for (const name in controllers) {
-            if (controllers.hasOwnProperty(name)) {
-                const controller = (controllers as any)[name];
-                ctlrInstances.push(new controller());
-            }
-        }
+        const controllers = new Array<any>();
+		
+		controllers.push(new AttentionController());
+		
         super.addControllers(ctlrInstances);
 	}
 	
